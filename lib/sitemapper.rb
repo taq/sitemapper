1b210483373df10bf21e0055e1c1c456817a9295
Eustaquio Rangel <taq@eustaquiorangel.com>
2012-01-27 17:38:35 -0200

    Calling methods when responding

--- lib/sitemapper.rb
+++ lib/sitemapper.rb
@@ -3,20 +3,22 @@ require "open-uri"
 
 module SiteMapper
    module ClassMethods
-      def write_sitemap(sitemap,method,extras=nil,extra_meth=nil)
+      def write_sitemap(sitemap,meth,extras=nil,extra_meth=nil)
          xml         = Builder::XmlMarkup.new(:indent=>2)
-         change      = sitemapper[:changefreq]
-         priority    = sitemapper[:priority]
-         extra_meth  = extra_meth ? extra_meth : method
-         col         = send(method)
+         extra_meth  = extra_meth ? extra_meth : meth
+         col         = send(meth)
          col         = col + extras.send(extra_meth) if extras
          xml.urlset(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9") do
             col.each do |value|
+               loc      = value.respond_to?(sitemapper[:loc])        ? value.send(sitemapper[:loc])        : value[sitemapper[:loc]]
+               lastmod  = value.respond_to?(sitemapper[:lastmod])    ? value.send(sitemapper[:lastmod])    : value[sitemapper[:lastmod]]
+               change   = value.respond_to?(sitemapper[:changefreq]) ? value.send(sitemapper[:changefreq]) : value[sitemapper[:changefreq]]
+               priority = value.respond_to?(sitemapper[:priority])   ? value.send(sitemapper[:priority])   : value[sitemapper[:priority]]
                xml.url do
-                  xml.loc        value[sitemapper[:loc]]
-                  xml.lastmod    value[sitemapper[:lastmod]]
-                  xml.changefreq value[change]   ? value[change] : change
-                  xml.priority   value[priority] ? value[priority] : priority
+                  xml.loc        loc
+                  xml.lastmod    lastmod 
+                  xml.changefreq change
+                  xml.priority   priority
                end                  
             end
          end
