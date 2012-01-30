bfbebbfb9402ea6e987b776e1270a63b26f0f44b
Eustaquio Rangel <taq@eustaquiorangel.com>
2012-01-30 12:08:50 -0200

    Converted tests to minitest.

--- test/taq-sitemapper_test.rb
+++ test/taq-sitemapper_test.rb
@@ -1,7 +1,8 @@
 require "rubygems"
 require "nokogiri"
-require "test/unit"
 require "active_record"
+require "minitest/spec"
+require "minitest/autorun"
 require "#{File.expand_path(File.dirname(__FILE__))}/../lib/taq-sitemapper.rb"
 
 ActiveRecord::Base.establish_connection({ 
@@ -11,97 +12,156 @@ ActiveRecord::Base.establish_connection({
 
 class SitemapperTestAR < ActiveRecord::Base  
    include SiteMapper
+
    sitemap[:changefreq] = :freq
    sitemap[:priority] = :priority
+
    def self.sitemap_extra
-      [{loc: "One"  , lastmod: Time.now, freq: "yearly", priority: 0.5}, 
-       {loc: "Two"  , lastmod: Time.now, freq: "yearly", priority: 0.5}, 
-       {loc: "Three", lastmod: Time.now, freq: "yearly", priority: 0.5}]
+      [{loc: "Four"  , lastmod: Time.now, freq: "yearly", priority: 0.5}, 
+       {loc: "Five"  , lastmod: Time.now, freq: "yearly", priority: 0.5}] 
    end
    def loc
       url
    end
 end
 
-class SiteMapperTest < Test::Unit::TestCase
-   def setup
-      @arraytest = [{url: "One"  , lastmod: Time.now, freq: "monthly", priority: 1}, 
-                    {url: "Two"  , lastmod: Time.now, freq: "monthly", priority: 1}, 
-                    {url: "Three", lastmod: Time.now, freq: "monthly", priority: 1}]
+def get_xml_doc(file)   
+   Nokogiri::XML(File.open(file))
+end
 
-      @sitemapper = SiteMapper::SiteMapper.new
-      @sitemapper.url = "http://localhost.com/sitemap.xml"
-      @sitemapper.loc = :url
-      @sitemapper.changefreq = :freq
-      @sitemapper.priority = :priority
-      @sitemapper.file = "/tmp/sitemap.xml"
-   end   
+def get_elements_num(file,element)
+   get_xml_doc(file).search(element).size
+end
 
-   def test_write_sitemap
-      File.unlink(@sitemapper.file) if File.exists?(@sitemapper.file)
-      @sitemapper.write_sitemap(@arraytest.entries) 
-      assert File.exists?(@sitemapper.file), "sitemap file not found"
+def delete_and_write(file,object,data=nil,extras=nil)
+   File.unlink(file) if File.exists?(file)
+   object.send(:write_sitemap,data,extras)
+end
+
+describe SiteMapper do
+   before do
+      @data =   [{url: "One"  , lastmod: Time.now, freq: "monthly", priority: 1}, 
+                 {url: "Two"  , lastmod: Time.now, freq: "monthly", priority: 1}, 
+                 {url: "Three", lastmod: Time.now, freq: "monthly", priority: 1}]
+      @extras = [{url: "Four" , lastmod: Time.now, freq: "yearly", priority: 0.5}, 
+                 {url: "Five" , lastmod: Time.now, freq: "yearly", priority: 0.5}]
    end
 
-   def test_url_size
-      test_write_sitemap
-      doc = Nokogiri::XML(File.open(@sitemapper.file))
-      assert_equal @arraytest.entries.size, doc.search("url").size
+   describe "Class option" do
+      before do
+         @sitemapper = SiteMapper::SiteMapper.new
+         @sitemapper.url = "http://localhost.com/sitemap.xml"
+         @sitemapper.loc = :url
+         @sitemapper.changefreq = :freq
+         @sitemapper.priority = :priority
+         @sitemapper.file = "/tmp/sitemap.xml"
+      end
+
+      describe "when asked to write the sitemap file" do
+         it "should write it with no extra options" do
+            delete_and_write(@sitemapper.file,@sitemapper,@data.entries)
+            assert File.exists?(@sitemapper.file), "sitemap file not found"
+         end
+
+         it "should write it with extra options" do
+            delete_and_write(@sitemapper.file,@sitemapper,@data.entries,@extras.entries)
+            assert File.exists?(@sitemapper.file), "sitemap file not found"
+         end
+      end
+
+      describe "when the sitemap file is written" do
+         before do
+            delete_and_write(@sitemapper.file,@sitemapper,@data.entries,@extras.entries)
+            @sitemapper.write_sitemap(@data.entries,@extras.entries) 
+         end
+
+         it "should have the correct url size" do
+            get_elements_num(@sitemapper.file,"url").must_equal @data.size+@extras.size, 
+         end
+
+         it "should have the correct loc size" do
+            get_elements_num(@sitemapper.file,"url").must_equal @data.size+@extras.size 
+         end
+
+         it "should have the correct changefreq size" do
+            get_elements_num(@sitemapper.file,"changefreq").must_equal @data.size+@extras.size
+         end
+
+         it "should have the correct lastmod size" do
+            get_elements_num(@sitemapper.file,"lastmod").must_equal @data.size+@extras.size 
+         end
+
+         it "should have the correct priority size" do
+            get_elements_num(@sitemapper.file,"priority").must_equal @data.size+@extras.size
+         end
+
+         it "should write the correct locs" do
+            elements = get_xml_doc(@sitemapper.file).search("loc")
+            for item in (@extras+@data)
+               elements.shift.text.must_equal item[:url]
+            end
+         end
+
+         it "should write the correct lastmod" do
+            elements = get_xml_doc(@sitemapper.file).search("lastmod")
+            for item in (@extras+@data)
+               elements.shift.text.must_equal item[:lastmod].to_s
+            end
+         end
+
+         it "should write the correct changefreq" do
+            elements = get_xml_doc(@sitemapper.file).search("changefreq")
+            for item in (@extras+@data)
+               elements.shift.text.must_equal item[:freq]
+            end
+         end
+
+         it "should write the correct priority" do
+            elements = get_xml_doc(@sitemapper.file).search("priority")
+            for item in (@extras+@data)
+               elements.shift.text.must_equal item[:priority].to_s
+            end
+         end
+      end
    end
 
-   def test_write_sitemap_with_extras
-      File.unlink(@sitemapper.file) if File.exists?(@sitemapper.file)
-      extras = [{url: "Four", lastmod: Time.now, freq: "yearly", priority: 0.5}, 
-                {url: "Five", lastmod: Time.now, freq: "yearly", priority: 0.5}]
-      @sitemapper.write_sitemap(@arraytest.entries,extras.entries) 
-      doc = Nokogiri::XML(File.open(@sitemapper.file))
-      assert_equal @arraytest.entries.size+extras.size, doc.search("url").size
-   end
+   describe "Module option" do
+      describe "when asked to write the sitemap file"  do
+          it "show write with no extras" do
+            delete_and_write(SitemapperTestAR.sitemap[:file],SitemapperTestAR)
+            assert File.exists?(SitemapperTestAR.sitemap[:file]), "sitemap file not found"
+          end
 
-   def test_ar_class_methods
-      assert_respond_to SitemapperTestAR, :write_sitemap
-      assert_respond_to SitemapperTestAR, :ping
-   end
+          it "show write with extras" do
+            SitemapperTestAR.sitemap[:extra] = :sitemap_extra
+            delete_and_write(SitemapperTestAR.sitemap[:file],SitemapperTestAR)
+            assert File.exists?(SitemapperTestAR.sitemap[:file]), "sitemap file not found"
+          end
+      end
 
-   def test_ar_class_attrs
-      assert_respond_to SitemapperTestAR, :sitemap
-   end
+      describe "when asked about the attributes" do
+         it "should have the module base methods" do
+            SitemapperTestAR.must_respond_to :write_sitemap
+            SitemapperTestAR.must_respond_to :ping
+         end
 
-   def test_class_collection
-      assert_equal :all, SitemapperTestAR.sitemap[:collection]
-   end
+         it "should have the sitemap attribute" do
+            SitemapperTestAR.must_respond_to :sitemap
+         end
 
-   def test_class_collection
-      assert_nil SitemapperTestAR.sitemap[:extra]
-   end
-   
-   def test_rw_class_attrs
-      str = "yearly"
-      SitemapperTestAR.sitemap[:changefreq] = str
-      assert_equal str, SitemapperTestAR.sitemap[:changefreq]
-   end
+         it "should have the all method as the collection method" do
+            SitemapperTestAR.sitemap[:collection].must_equal :all
+         end
 
-   def test_class_methods_default_values
-      assert_equal :freq, SitemapperTestAR.sitemap[:changefreq]
-   end
+         it "should have the extra collection with the right value" do
+            SitemapperTestAR.sitemap[:extra].must_be_nil
+         end
 
-   def test_write_sitemap_on_class
-      file = SitemapperTestAR.sitemap[:file]
-      File.unlink(file) if File.exists?(file)
-      SitemapperTestAR.write_sitemap
-      assert File.exists?(file), "sitemap file not found"
-      doc = Nokogiri::XML(File.open(file))
-      assert_equal SitemapperTestAR.all.size, doc.search("url").size
-   end
-
-   def test_write_sitemap_on_class_with_extras
-      file = SitemapperTestAR.sitemap[:file]
-      File.unlink(file) if File.exists?(file)
-      SitemapperTestAR.sitemap[:extra] = :sitemap_extra
-  
-      SitemapperTestAR.write_sitemap
-      assert File.exists?(file), "sitemap file not found"
-      doc = Nokogiri::XML(File.open(file))
-      assert_equal SitemapperTestAR.all.size+3, doc.search("url").size
+         it "should read and write on attributes" do
+            str = "yearly"
+            SitemapperTestAR.sitemap[:changefreq] = str
+            SitemapperTestAR.sitemap[:changefreq].must_equal str
+         end
+      end
    end
 end
